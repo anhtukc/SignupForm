2547b8b3b9f605a8c14e65f49e09f6fa43584fc9
anhtudev <anhtukc2.0@gmail.com>
2022-12-13 16:46:56 +0700

    Add project files.

---
+++ WebApplication4/Controllers/AuthenticationController.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication4.Models;
+
+namespace WebApplication4.Controllers
+{
+    public class AuthenticationController : Controller
+    {
+     
+        public ActionResult Index()
+        {          
+            return View();
+        }
+
+        public ActionResult SignUp(Customer cus)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Details", cus);
+            }
+            return View("Index", cus);
+        }
+        public ActionResult Details(Customer cus)
+        {
+            return View(cus);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Authentication/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Authentication/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Authentication/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Authentication/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Authentication/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ WebApplication4/Models/Customer.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication4.Models
+{
+    public class Customer
+    {
+        [Required(ErrorMessage = "Please enter first name.")]
+        public string firstName { get; set; }
+
+        [Required(ErrorMessage = "Please enter last name.")]
+        public string lastName { get; set; }
+
+        [Required(ErrorMessage = "Please enter email address.")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string emailAddress { get; set; }
+
+        [Required(ErrorMessage = "Please enter password.")]
+        [MinLength(8, ErrorMessage = "Password must have at least 8 characters.")]
+        public string password { get; set; }
+
+
+        public string country { get; set; }
+
+
+        //[Compare( ErrorMessage = "You must accept the terms and conditions")]
+      
+        [EnforceTrue(ErrorMessage = @"You must accept the terms and conditions")]
+        public bool AcceptsTerms { get; set; } = false;
+        public Customer(string firstName, string lastName, string emailAddress, string password, string country)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.emailAddress = emailAddress;
+            this.password = password;
+            this.country = country;
+        }
+        public Customer()
+        {
+
+        }
+    }
+}
+
+public class EnforceTrueAttribute : ValidationAttribute, IClientValidatable
+{
+    public override bool IsValid(object value)
+    {
+        if (value == null) return false;
+        if (value.GetType() != typeof(bool)) throw new InvalidOperationException("can only be used on boolean properties.");
+        return (bool)value == true;
+    }
+
+    public override string FormatErrorMessage(string name)
+    {
+        return "The " + name + " field must be checked in order to continue.";
+    }
+
+    public IEnumerable<ModelClientValidationRule> GetClientValidationRules(ModelMetadata metadata, ControllerContext context)
+    {
+        yield return new ModelClientValidationRule
+        {
+            ErrorMessage = String.IsNullOrEmpty(ErrorMessage) ? FormatErrorMessage(metadata.DisplayName) : ErrorMessage,
+            ValidationType = "enforcetrue"
+        };
+    }
+}
